f0858f65a44fab452f7b510f2f0d3c7735634fc6
Quique Fdez Guerra <quique@ckgrafico.com>
2019-08-08 20:48:10 +0200

    Add menubar

--- public/electron.js
+++ public/electron.js
@@ -1,28 +1,38 @@
-const electron = require("electron");
+const electron = require('electron');
 const app = electron.app;
 const BrowserWindow = electron.BrowserWindow;
-const path = require("path");
-const isDev = require("electron-is-dev");
-let mainWindow;
+const path = require('path');
+const isDev = require('electron-is-dev');
+const { menubar } = require('menubar');
 
-function createWindow() {
-  mainWindow = new BrowserWindow({ width: 900, height: 680 });
-  mainWindow.loadURL(
-    isDev
-      ? "http://localhost:3000"
-      : `file://${path.join(__dirname, "../build/index.html")}`
-  );
-  mainWindow.on("closed", () => (mainWindow = null));
-}
-app.on("ready", createWindow);
+const APP_WIDTH = 320;
+const APP_HEIGHT = 568;
 
-app.on("window-all-closed", () => {
-  if (process.platform !== "darwin") {
+const mb = menubar({
+  width: APP_WIDTH,
+  height: APP_HEIGHT,
+  'preload-window': true,
+  resizable: false,
+  browserWindow: {
+    width: APP_WIDTH,
+    height: APP_HEIGHT
+  },
+  index: isDev
+    ? 'http://localhost:3000'
+    : `file://${path.join(__dirname, '../build/index.html')}`
+});
+
+mb.on('after-create-window', () => {
+  mb.window.openDevTools();
+});
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
     app.quit();
   }
 });
 
-app.on("activate", () => {
+app.on('activate', () => {
   if (mainWindow === null) {
     createWindow();
   }
